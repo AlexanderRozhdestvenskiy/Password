597769d9a013f26f60b1e2baf38deb05a01aaaf9
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2022-03-31 12:36:18 +0300

    StructPasswordCriteria

---
+++ Password/PasswordCriteria.swift
@@ -0,0 +1,22 @@
+//
+//  PasswordCriteria.swift
+//  Password
+//
+//  Created by Alexander Rozhdestvenskiy on 31.03.2022.
+//
+
+import Foundation
+
+struct PasswordCriteria {
+    static func lengthCriteriaMet(_ text: String) -> Bool {
+        text.count >= 8 && text.count <= 32
+    }
+    
+    static func noSpaceCriteriaMet(_ text: String) -> Bool {
+        text.rangeOfCharacter(from: NSCharacterSet.whitespaces) == nil
+    }
+    
+    static func lengthAndSpaceMet(_ text: String) -> Bool {
+        lengthCriteriaMet(text) && noSpaceCriteriaMet(text)
+    }
+}
